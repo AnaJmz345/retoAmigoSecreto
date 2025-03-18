1e712a0661e622d33542aee1e5ac265030c911b5
AnaJmz345 <anajmzse@gmail.com>
2025-03-17 18:58:47 -0600

    "base de la función agregar amigo"

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigosIngresados=[];
+function agregarAmigo(){
+    amigoNuevo = document.getElementById("amigo").value;
+    amigosIngresados.push(amigoNuevo)
+    console.log(amigosIngresados)
+    const listaAmigos = document.getElementById("listaAmigos")
+    const nuevoAmigoLi = document.createElement("li");
+    nuevoAmigoLi.textContent = amigoNuevo
+    listaAmigos.appendChild(nuevoAmigoLi)
+}
+
+function sortearAmigo(){
+
+}
